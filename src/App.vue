e5ed89efdfbd5af66db2e953e20bffa56f57739f
karolkol <karol.prx@o2.pl>
2020-04-19 11:11:22 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <Header></Header>
+  <v-content>
+    <Header></Header>
+    <Footer></Footer>
+  </v-content>
 </template>
 
 <script>
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Footer
   },
 
-  data: () => ({
-    
-  })
+  data: () => ({})
 };
 </script>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-footer :dark="$vuetify.theme.dark">
+    <v-spacer></v-spacer>
+    <div>&copy; USOS {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
