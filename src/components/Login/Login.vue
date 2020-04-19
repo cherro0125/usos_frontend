151ac403f1936ba76ac8ba179a84333b648f67ef
karolkol <karol.prx@o2.pl>
2020-04-19 13:12:43 +0200

    Login page view

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-content>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="8">
+          <v-form @submit.prevent="login()">
+            <v-card class="signin-card pa-8">
+              <v-card-title class="justify-center signin-card-title">
+                <h2>Log in</h2>
+              </v-card-title>
+              <v-card-text>
+                <v-text-field
+                  type="text"
+                  required
+                  v-model="credentials.username"
+                  placeholder="Login"
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  required
+                  v-model="credentials.password"
+                  placeholder="Password"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions class="justify-center">
+                <v-btn type="submit">Log In</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      credentials: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Grades from './components/Grades/Grades';
+import Login from './components/Login/Login';
 
 Vue.use(Router);
 
 const router = new Router({
     mode: 'history',
     routes: [
+        {
+            path: '/login',
+            component: Login
+        },
         {
             path: '/grades',
             component: Grades
