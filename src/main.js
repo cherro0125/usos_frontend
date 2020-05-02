d2a2ec0580b809f7f9385c4d0cc24314d8b01b00
karolkol <karol.prx@o2.pl>
2020-05-02 18:20:52 +0200

    Add missing axios import

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
-import store from './store/store'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import store from './store/store';
+import axios from 'axios';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 axios.defaults.baseURL = process.env.BASE_URL;
 
