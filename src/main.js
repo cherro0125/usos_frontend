8b9ab1fff131535e7d0673315a0ba7c14e88f0d3
karolkol <karol.prx@o2.pl>
2020-05-02 18:11:12 +0200

    Set axios base url

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import store from './store/store'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
+axios.defaults.baseURL = process.env.BASE_URL;
+
+const token = localStorage.getItem('token');
+if (token) {
+  axios.defaults.headers.common['Autorization'] = 'Bearer ' + token;
+}
+
 Vue.config.productionTip = false
 
 new Vue({
