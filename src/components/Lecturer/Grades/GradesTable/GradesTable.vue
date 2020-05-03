170bc5beb730a92bacea02802a275073ed8b7a4a
karolkol <karol.prx@o2.pl>
2020-05-03 21:01:48 +0200

    Grades table caloumn names, save and cancel buttons

--- src/components/Lecturer/Grades/GradesTable/GradesTable.vue
+++ src/components/Lecturer/Grades/GradesTable/GradesTable.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container>
-    <v-data-table
-      hide-default-footer
-      :dark="$vuetify.theme.dark"
-      :headers="headers"
-      class="elevation-1"
-    ></v-data-table>
+    <v-card>
+      <v-data-table
+        hide-default-footer
+        :dark="$vuetify.theme.dark"
+        :headers="headers"
+        class="elevation-1"
+      ></v-data-table>
+      <v-card-actions class="justify-center">
+         <v-btn to="/lecturer/grades">cancel</v-btn>
+        <v-btn>save</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
@@ -14,9 +20,11 @@ export default {
   data() {
     return {
       headers: [
-        { text: "Subject", value: "name" },
-        { text: "Mark", value: "mark" },
-        { text: "Ects", value: "ects" }
+        { text: "Index", value: "index" },
+        { text: "Name", value: "name" },
+        { text: "Surname", value: "surname" },
+        { text: "I Term", value: "firstTerm" },
+        { text: "II Term", value: "secondTerm" }
       ]
     };
   }
