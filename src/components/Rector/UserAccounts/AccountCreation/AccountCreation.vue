3ef95b19bd6753908e6ee5d95f3efaf96d735f48
karolkol <karol.prx@o2.pl>
2020-05-17 15:24:23 +0200

    Add fields to user registration form

--- src/components/Rector/UserAccounts/AccountCreation/AccountCreation.vue
+++ src/components/Rector/UserAccounts/AccountCreation/AccountCreation.vue
@@ -1,11 +1,48 @@
 <template>
   <v-container>
+    <h2 class="ml-2 mb-5">User registration</h2>
     <v-form @submit.prevent="createAccount()">
       <v-card class="pa-10">
-        <v-text-field v-model="username" label="email"></v-text-field>
-        <v-select v-model="role" label="role" class="mt-5" :items="roles" outlined></v-select>
+        <v-card-title>Personal Data</v-card-title>
+        <v-text-field v-model="userData.firstName" label="name"></v-text-field>
+        <v-text-field v-model="userData.lastName" label="surname"></v-text-field>
+        <v-text-field v-model="userData.phoneNumber" label="phone number"></v-text-field>
+        <v-text-field v-model="userData.username" label="email"></v-text-field>
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="userData.dateOfBirth"
+              label="birthday date"
+              prepend-icon="event"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            ref="picker"
+            v-model="userData.dateOfBirth"
+            :max="new Date().toISOString().substr(0, 10)"
+            min="1950-01-01"
+            @change="save"
+          ></v-date-picker>
+        </v-menu>
+        <v-card-title>Address</v-card-title>
+        <v-text-field v-model="userData.city" label="city"></v-text-field>
+        <v-text-field v-model="userData.street" label="street"></v-text-field>
+        <v-text-field v-model="userData.streetNumber" label="street number"></v-text-field>
+        <v-text-field v-model="userData.houseNumber" label="house number"></v-text-field>
+        <v-text-field v-model="userData.zipCode" label="zip code"></v-text-field>
+        <v-card-title>Role</v-card-title>
+        <v-select v-model="userData.role" label="role" class="mt-5" :items="roles" outlined></v-select>
         <v-card-actions class="justify-center">
-          <v-btn type="submit">Create Account</v-btn>
+          <v-btn type="submit">Create an account</v-btn>
         </v-card-actions>
       </v-card>
     </v-form>
@@ -18,20 +55,31 @@ import { mapActions } from "vuex";
 export default {
   data() {
     return {
-      roles: ["student", "lecturer", "dean"],
-      role: "student",
-      username: ''
+      roles: ["STUDENT", "LECTURER", "DEAN"],
+      menu: false,
+      userData: {
+        role: "STUDENT",
+        username: "",
+        dateOfBirth: "",
+        firstName: "",
+        lastName: "",
+        password: "test",
+        phoneNumber: '',
+        city: '',
+        street: '',
+        streetNumber: '',
+        houseNumber: '',
+        zipCode: ''
+      }
     };
   },
   methods: {
-    ...mapActions(['register']),
+    ...mapActions(["register"]),
     async createAccount() {
-        const params = {
-            'username': this.username,
-            'password': 'test',
-            'role': this.role.toUpperCase()
-        };
-        await this.register(params);
+      await this.register(this.userData);
+    },
+    save(date) {
+      this.$refs.menu.save(date);
     }
   }
 };
