d6289073cd550a750aa09df37b563d2c20b71629
karolkol <karol.prx@o2.pl>
2020-05-30 14:36:35 +0200

    Add news modal

---
+++ src/components/News/AddNews/AddNews.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="addNewsModal" persistent max-width="800">
+      <v-card class="pa-12">
+        <v-card-title class="headline justify-center">Add news</v-card-title>
+        <v-text-field v-model="newsTitle" label="title"></v-text-field>
+        <v-textarea v-model="newsDescription" name="input-7-1" auto-grow row-height="5vh" label="description"></v-textarea>
+        <v-card-actions class="justify-center">
+          <v-btn text @click="$emit('update:addNewsModal', false)">Cancel</v-btn>
+          <v-btn text  @click="addNews">Add</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newsTitle: "",
+      newsDescription: ""
+    };
+  },
+  props: ["addNewsModal"],
+  methods: {
+    addNews() {
+      const data = {
+        title: this.newsTitle,
+        description: this.newsDescription
+      };
+      this.$emit("addNews", data);
+      this.$emit("update:addNewsModal", false)
+    }
+  }
+};
+</script>
--- src/components/News/News.vue
+++ src/components/News/News.vue
@@ -1,6 +1,15 @@
 <template>
   <v-container>
-    <h2 class="pl-5 mb-5">News</h2>
+    <v-row>
+      <h2 class="pl-5 mb-5">News</h2>
+      <v-spacer></v-spacer>
+      <v-btn
+        color="primary mr-5"
+        dark
+        @click.stop="addNewsModal = true"
+        v-if="this.role === 'DEAN'"
+      >Add news</v-btn>
+    </v-row>
     <p class="pa-5">
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -8,11 +17,32 @@
       velit esse cillum dolore eu fugiat nulla pariatur.
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     </p>
+    <AddNewsModal v-if="this.role === 'DEAN'" :addNewsModal.sync="addNewsModal" @addNews="addNews">
+    </AddNewsModal>
   </v-container>
 </template>
 
 <script>
-export default {};
+import AddNewsModal from "./AddNews/AddNews";
+
+export default {
+  data() {
+    return {
+      role: "",
+      addNewsModal: false
+    };
+  },
+  components: { AddNewsModal },
+  created() {
+    this.role = localStorage.getItem("role");
+  },
+  methods: {
+    addNews(data) {
+      console.log(data)
+      this.addNewsModal = false;
+    }
+  }
+};
 </script>
 
 <style scoped>
