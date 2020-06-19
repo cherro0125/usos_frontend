edc28f6bf9dc8789554d0d37c187732156cfebe5
karolkol <karol.prx@o2.pl>
2020-06-19 19:27:39 +0200

    Add pay btn in payments table

--- src/components/Student/Payments/StudentPayments.vue
+++ src/components/Student/Payments/StudentPayments.vue
@@ -1,7 +1,12 @@
 <template>
   <v-container>
     <h2 class="ml-4 mb-8">Payments</h2>
-    <v-data-table hide-default-footer :headers="headers" :items="payments" class="elevation-1"></v-data-table>
+    <v-data-table hide-default-footer :headers="headers" :items="payments" class="elevation-1">
+      <template v-slot:item.actions="{ item }">
+        <v-btn v-if="item.status === 'NEW' || item.status === 'CREATED'" small class="mr-2" @click="pay(item.id)">pay</v-btn>
+        <hr class="line" v-else>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
@@ -14,13 +19,17 @@ export default {
         { text: "type", value: "purposeType" },
         { text: "payable to", value: "payableTo" },
         { text: "description", value: "details" },
-        { text: "status", value: "status" }
+        { text: "status", value: "status" },
+        { text: "actions", value: "actions" }
       ]
     };
   },
   computed: mapGetters(["payments"]),
   methods: {
-    ...mapActions(["getPaymentsByPayerId"])
+    ...mapActions(["getPaymentsByPayerId"]),
+    async pay(item) {
+      console.log(item)
+    }
   },
   async created() {
     const id = localStorage.getItem("userId");
@@ -30,4 +39,7 @@ export default {
 </script>
 
 <style scoped>
+ .line {
+   width: 55px;
+ }
 </style>
