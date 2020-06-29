70d141eae2face4644b9bd5c6d42a679944af013
Kasiaak <zuzanna.dawiec@gmail.com>
2020-06-29 17:58:59 +0100

    Delete temporary method templates

--- src/store/modules/porter.js
+++ src/store/modules/porter.js
@@ -48,13 +48,6 @@ const actions = {
         else
             console.log(err)
     },
-    async temp({ commit }) {
-        const { res, err } = await promiseWrapper(axios.get(`/keys/all/${localStorage.getItem("userId")}`));
-        if (res)
-            commit('setCourseData', res.data);
-        else
-            console.log(err)
-    },
     async giveKeys({ commit }, data) {
         const { res, err } = await promiseWrapper(axios.post('/keys/giveKeys', data));
         if (res)
@@ -68,37 +61,6 @@ const actions = {
             console.log(res)
         if (err)
             console.log(err)
-    },
-    async getTGradesValues({ commit }) {
-        const { res, err } = await promiseWrapper(axios.get('util/enum/GradeValueType/values'));
-        if (res)
-            commit("setGradesValues", res.data.enumValues);
-        else
-            console.log(err);
-    },
-    async getTCourseStudentsData({ commit }, data) {
-        const { id, course, students } = data;
-        const { res, err } = await promiseWrapper(axios.get(`/grade/lecturer/${id}`));
-        if (res) {
-            const courseData = res.data.filter(element => element.courseGroup.name === course)
-            const courseStudentsData = students;
-            courseStudentsData.forEach(element => {
-                const firstTerm = courseData.find(el => el.examDateType === "FIRST" && element.id === el.assignedUser.id);
-                const secondTerm = courseData.find(el => el.examDateType === "SECOND" && element.id === el.assignedUser.id);
-                element.firstTerm = firstTerm ? firstTerm.value : "";
-                element.secondTerm = secondTerm ? secondTerm.value : "";
-                element.firstTermId = firstTerm ? firstTerm.id : "";
-                element.secondTermId = secondTerm ? secondTerm.id : "";
-            });
-            commit("setCourseStudentsData", courseStudentsData);
-        }
-        else
-            console.log(err)
-    },
-    async removeTGrade({ commit }, id) {
-        const { res, err } = await promiseWrapper(axios.delete(`/grade/${id}/delete`));
-
-        return res ? true : false;
     },
     emptyRoomKeys() {
         state.roomKeys = []
